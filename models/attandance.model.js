f35b20ff02b185bc356ffa4ad655f3f837a4bd79
AYUSHMISHRA2003 <ayushm185@gmail.com>
2025-03-22 13:08:52 +0530

    small changes done

--- models/attandance.model.js
+++ models/attandance.model.js
@@ -3,9 +3,16 @@ import { Schema } from "mongoose";
 
 const attandanceSchema=new Schema(
     {
-
+        data:{
+            type:String,
+            required:true
+        },
+        p1:{
+            type:String,
+            required:false
+        }
     },
     {
-        
+
     }
 )
