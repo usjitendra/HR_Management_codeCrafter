e03a06f87ff5b6931b55c72c5afeb62ae8dd5831
Ankit Yadav <ankitjiyadav@example.com>
2025-04-02 17:07:24 +0530

    update

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app=express();
 dotenv.config();
 
 app.use(cors({
-  origin: ["http://localhost:5173","https://hrmsdashboard4.netlify.app","http://localhost:3000"], 
+  origin: ["http://localhost:5173","https://hrmsdashboard4.netlify.app","http://localhost:3001"], 
   credentials: true,
 }));
 
