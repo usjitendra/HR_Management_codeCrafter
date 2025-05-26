0b62d212f1055c27d82e39706b1b1df9516a396c
Ankit Yadav <ankitjiyadav@example.com>
2025-05-26 16:26:39 +0530

    update

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -80,7 +80,6 @@ const login = async (req, res, next) => {
         }
          const token=await generate_Token(loginData);
          await registrationModel.findByIdAndUpdate(loginData._id, { token });
-        //  await employeModel.findOneAndUpdate({registrationId:loginData.id,fcmToken:fcmToken});
          const employeeeData=await employeModel.findOne({registrationId:loginData.id})
         //  console.log(employeeeData);
          res.cookie("authToken", token, {
