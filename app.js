1a81d476c1255fedf13ae24e2b150612ea7a5e91
Ankit Yadav <ankitjiyadav@example.com>
2025-06-13 19:34:43 +0530

    update

--- app.js
+++ app.js
@@ -38,15 +38,15 @@ const app=express();
 dotenv.config();
 
 app.use(cors({
-  origin: ["http://localhost:5173","http://localhost:3000","https://hrms112.netlify.app","https://dr-monika.netlify.app"], 
-  origin: ["http://localhost:5173","http://localhost:3000","https://hrms112.netlify.app","http://localhost:5174","sadbhawanaclinic.com","https://www.sadbhawanaclinic.com/", "https://sadbhawanaclinic.com/"], 
+  origin: ["http://localhost:5173","http://localhost:3000","https://hrms112.netlify.app","updatehrma.netlify.app","https://dr-monika.netlify.app"], 
+  origin: ["http://localhost:5173","http://localhost:3000","https://hrms112.netlify.app","updatehrma.netlify.app","http://localhost:5174","sadbhawanaclinic.com","https://www.sadbhawanaclinic.com/", "https://sadbhawanaclinic.com/"], 
   credentials: true,
 }));
 
 const server=http.createServer(app);
 const io=new Server(server,{
        cors:{
-        origin:["http://localhost:5173","http://localhost:3000","https://hrms112.netlify.app"],
+        origin:["http://localhost:5173","http://localhost:3000","https://hrms112.netlify.app","updatehrma.netlify.app"],
         credentials: true,
        }
 })
